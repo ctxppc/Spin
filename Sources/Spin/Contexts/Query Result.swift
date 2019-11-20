1a8999ae5d2b01e143a9078bc172233979ae8b35
Constantino Tsarouhas <c@tsarouhas.eu>
2019-11-20 22:15:42 +0100

    Simplified Query Result.

--- Sources/Spin/Contexts/Query Result.swift
+++ Sources/Spin/Contexts/Query Result.swift
@@ -1,18 +1,19 @@
 // Spin © 2019 Constantino Tsarouhas
 
-import Vapor
+import Fluent
 
 /// A dynamic property that evaluates to the result of a database query.
 @propertyWrapper
-public struct QueryResult<Formula : QueryFormula> : DynamicProperty {
+public struct QueryResult<Result> : DynamicProperty {
 	
-	/// Creates a query result with given query formula.
-	public init(_ query: Formula) {
+	/// Creates a query result with given query.
+	public init(_ query: @escaping Query) {
 		self.query = query
 	}
 	
 	/// The formula of the query to evaluate.
-	public let query: Formula
+	private let query: Query
+	public typealias Query = (DatabaseConnectable) -> Future<Result>
 	
 	/// The result of the query.
 	public var wrappedValue: Result {
@@ -22,11 +23,10 @@ public struct QueryResult<Formula : QueryFormula> : DynamicProperty {
 	
 	/// The result of the query, or `nil` if the query result hasn't been prepared.
 	private var storedResult: Result?
-	public typealias Result = Formula.Result
 	
 	// See protocol.
 	public func prepareForRendering(by renderer: Renderer) -> Future<Self> {
-		query.result(on: renderer.request).map { result in
+		query(renderer.request).map { result in
 			var copy = self
 			copy.storedResult = result
 			return copy
--- Sources/Spin/Function Evaluation.swift
+++
@@ -1,7 +0,0 @@
-// Spin © 2019 Constantino Tsarouhas
-
-infix operator •>
-
-func •> <A, B, C>(first: @escaping (A) -> B, second: @escaping (B) -> C) -> (A) -> C {
-	{ second(first($0)) }
-}

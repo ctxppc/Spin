e41687ee8c0ee698deca802117e5a76d12b8b0c3
Constantino Tsarouhas <c@tsarouhas.eu>
2019-12-21 17:31:37 +0100

    Added & implemented Emphasised.

---
+++ Sources/Spin/Common Components/Emphasised.swift
@@ -0,0 +1,38 @@
+// Spin © 2019 Constantino Tsarouhas
+
+/// A component representing an emphasised span of text or other content.
+public struct Emphasised<Contents : Component> : ElementComponent {
+	
+	/// Creates an emphasised span with given contents.
+	public init(strong: Bool = false, @ComponentBuilder contents: @escaping () -> Contents) {
+		self.strong = strong
+		self.contents = contents
+	}
+	
+	/// A Boolean value indicating whether strong emphasis is applied.
+	public var strong: Bool
+	
+	// See protocol.
+	public var tagName: String { strong ? "strong" : "em" }
+	
+	// See protocol.
+	public var classNames: Set<String> = []
+	
+	// See protocol.
+	public var attributes: [String : String] = [:]
+	
+	// See protocol.
+	public let contents: () -> Contents
+	
+}
+
+extension Emphasised where Contents == Text {
+	
+	/// Creates an emphasised span of text.
+	public init(strong: Bool = false, text: String) {
+		self.init(strong: strong) {
+			Text(text)
+		}
+	}
+	
+}

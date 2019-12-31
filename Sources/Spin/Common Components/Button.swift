7e4f658776612dca56b0f5c6fe2b16a44bfe9031
Constantino Tsarouhas <c@tsarouhas.eu>
2019-12-31 20:08:00 +0100

    Added support for differentiated actions to Button.

--- Sources/Spin/Common Components/Button.swift
+++ Sources/Spin/Common Components/Button.swift
@@ -3,8 +3,15 @@
 /// An actionable button in a document.
 public struct Button<Contents : Component> : ElementComponent {
 	
+	/// Creates a button with given contents.
 	public init(@ComponentBuilder contents: @escaping () -> Contents) {
 		self.contents = contents
+	}
+	
+	/// Creates a button performing given action and containing given contents.
+	public init<Action : DifferentiatedAction>(for _: Action.Type, kind: Action.Kind, @ComponentBuilder contents: @escaping () -> Contents) {
+		self.contents = contents
+		self.attributes = [Action.kindCodingKey.stringValue : kind.rawValue]
 	}
 	
 	// See protocol.
@@ -29,5 +36,22 @@ extension Button where Contents == Text {
 			Text(text)
 		}
 	}
+	
+	/// Creates a button performing given action and containing given text.
+	public init<Action : DifferentiatedAction>(for type: Action.Type, kind: Action.Kind, _ text: String) {
+		self.init(for: type, kind: kind) {
+			Text(text)
+		}
+	}
+	
+}
+
+public protocol DifferentiatedAction : ActionProtocol {
+	
+	/// A value specifying the kind of action to take.
+	associatedtype Kind : RawRepresentable where Kind.RawValue == String
+	
+	/// The key for encoding the kind.
+	static var kindCodingKey: CodingKeys { get }
 	
 }

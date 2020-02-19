cd290bb661b29cec686cf28ec7ef37a14f621172
Constantino Tsarouhas <c@tsarouhas.eu>
2020-02-19 09:30:07 +0100

    Added Context Provider. Implemented Realiser.

---
+++ Sources/SpinCore/Contexts/Context Provider.swift
@@ -0,0 +1,50 @@
+// Spin © 2019–2020 Constantino Tsarouhas
+
+import Vapor
+
+/// An object that can provide a component's context.
+public protocol ContextProvider : class {
+	
+	/// The context.
+	var context: Context { get }
+	
+}
+
+/// An object that manages a stack of context providers.
+public final class ContextProviderStack {
+	
+	/// The local stack.
+	public static var local: ContextProviderStack {
+		if let stack = localStorage.currentValue {
+			return stack
+		} else {
+			let stack = Self()
+			localStorage.currentValue = stack
+			return stack
+		}
+	}
+	
+	/// The storage for the local stacks.
+	private static let localStorage = ThreadSpecificVariable(value: ContextProviderStack())
+	
+	/// The current context provider.
+	public var current: ContextProvider? {
+		providers.last
+	}
+	
+	/// Adds given provider on the stack.
+	func push(_ provider: ContextProvider) {
+		providers.append(provider)
+	}
+	
+	/// Removes the innermost provider from the stack.
+	///
+	/// - Requires: The stack is not empty.
+	func pop() {
+		providers.removeLast()
+	}
+	
+	/// The providers on the stack, ordered from outermost to innermost.
+	private var providers: [ContextProvider] = []
+	
+}
---
+++ Sources/SpinCore/Contexts/Realiser.swift
@@ -0,0 +1,68 @@
+// Spin © 2019–2020 Constantino Tsarouhas
+
+import DepthKit
+import Vapor
+
+/// An object that manages a component's context.
+///
+/// A component's body must be evaluated within the scope of a realiser who manages any contextual and external properties the component might have.
+public final class Realiser<ComponentType : Component> : ContextProvider {
+	
+	/// Creates a realiser for given component and with given context.
+	init(for component: ComponentType, context: Context) {
+		
+		self.component = component
+		self.context = context
+		
+		let externalProperties = Mirror(reflecting: component)
+			.children
+			.compactMap { $0.value as? AnyExternalProperty }
+		
+		let completions = externalProperties.map { property -> Future<()> in
+			let description = property.typeErasedDescription
+			return description.value(context: context).map { result in
+				self.context.fulfill(description, with: result)
+			}
+		}
+		
+		if let eventLoop = completions.first?.eventLoop {
+			dependencyCompletion = Future.andAll(completions, eventLoop: eventLoop)
+		}
+		
+	}
+	
+	/// The component being realised.
+	public let component: ComponentType
+	
+	/// The component's context.
+	public private(set) var context: Context
+	
+	/// A future completion of all external property dependencies, or `nil` if the component doesn't have any dependencies.
+	private var dependencyCompletion: Future<()>?
+	
+	/// Performs a scoped access of the component.
+	///
+	/// The component's body can be accessed within (and only within) the provided closure.
+	///
+	/// This method is reentrant over all instances of `Self` including `self`, i.e., `withRealisedComponent(_:on:)` invocations can be nested. When accessing a nested component, it must be scoped using its own realiser.
+	public func withRealisedComponent<Result>(_ access: @escaping ScopedAccessor<Result>, on worker: Worker) -> Future<Result> {
+		
+		func performScopedAccess() throws -> Result {
+			ContextProviderStack.local.push(self)
+			defer { ContextProviderStack.local.pop() }
+			return try access(component)
+		}
+		
+		if let completion = dependencyCompletion {
+			return completion
+				.map(performScopedAccess)
+				.hopTo(eventLoop: worker.eventLoop)
+		} else {
+			return Future.map(on: worker, performScopedAccess)
+		}
+		
+	}
+	
+	public typealias ScopedAccessor<Result> = (ComponentType) throws -> Result
+	
+}

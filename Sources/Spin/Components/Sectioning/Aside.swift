13ef4a138e5629657454fea2c56ffe32cd73b9eb
Constantino Tsarouhas <c@tsarouhas.eu>
2021-09-28 22:45:27 +0200

    Adds Aside.

---
+++ Sources/Spin/Components/Sectioning/Aside.swift
@@ -0,0 +1,30 @@
+// Spin © 2019–2021 Constantino Tsarouhas
+
+import Conifer
+import DepthKit
+
+/// A component that groups components that form an aside.
+///
+/// A typical aside consists of a header (`Header`), body, and footer (`Footer`). Standalone text components in the body are presumed to be paragraphs of the aside.
+public struct Aside<Contents : Component> : Component {
+	
+	/// Creates an aside with given contents.
+	public init(@ComponentBuilder _ contents: @escaping ContentsProvider) {
+		self.contents = contents
+	}
+	
+	/// The aside's contents.
+	public let contents: ContentsProvider
+	public typealias ContentsProvider = () -> Contents
+	
+	// See protocol.
+	public var body: Never {
+		Never.hasNoBody(self)
+	}
+	
+	// See protocol.
+	public func render<G>(in graph: inout G, at location: ShadowGraphLocation) async where G : ShadowGraphProtocol {
+		TODO.unimplemented
+	}
+	
+}
